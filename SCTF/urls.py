c53442401a3abc64742fe3d0a6f352fdd685749d
Filippo <filippo.schiavio@gnucoop.com>
2017-05-03 18:28:38 +0200

    static urls

--- SCTF/urls.py
+++ SCTF/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^sctf/', include('sctf.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
