fb01305e67341cd0b05f3969d0337cb8e983e745
Filippo <filippo.schiavio@gnucoop.com>
2017-05-03 18:37:13 +0200

    static urls

--- SCTF/urls.py
+++ SCTF/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^sctf/', include('sctf.urls')),
     url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
