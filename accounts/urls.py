8de2ce9864624b6828294d95e6bc969a30e305e2
Filippo <filippo.schiavio@gnucoop.com>
2017-05-04 17:26:43 +0200

    ...

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,3 +4,6 @@ urlpatterns = [
     url(r'^', include('registration.backends.simple.urls')),
     url(r'^', include('registration.auth_urls')),
 ]
+
+import registration.backends.simple.urls
+import registration.auth_urls
