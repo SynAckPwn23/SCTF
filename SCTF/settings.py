6f6ff43ffa75e93fdb0d1fa5ae23f76bb88582cd
Filippo <filippo.schiavio@gnucoop.com>
2017-05-04 20:05:15 +0200

    ...

--- SCTF/settings.py
+++ SCTF/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = 'uploaded_media/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = reverse_lazy('auth_login')
