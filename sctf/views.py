022f17c59e855c2d4713ccfd866e130a33840022
Filippo <filippo.schiavio@gnucoop.com>
2017-05-04 16:51:05 +0200

    count teams

--- sctf/views.py
+++ sctf/views.py
@@ -2,11 +2,14 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from sctf.models import Team
+
 
 @login_required
 def index(request):
     parameters = {
         'users_count': get_user_model().objects.count(),
+        'teams_count': Team.objects.count(),
     }
     return render(request, 'base.html', parameters)
 
