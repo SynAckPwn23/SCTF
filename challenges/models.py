43b56891aff57b863ef38663bcac30e2ec98abea
Filippo <filippo.schiavio@gnucoop.com>
2017-05-04 18:20:25 +0200

    challenges models

--- challenges/models.py
+++ challenges/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=256)
+    category = models.ForeignKey('challenges.Category')
+    description = models.TextField()
+    key = models.CharField(max_length=256)
+    points = models.IntegerField()
+
+
+class Hint(models.Model):
+    challenge = models.ForeignKey('challenges.Challenge')
+    text = models.TextField
+
+
+class Attachment(models.Model):
+    challenge = models.ForeignKey('challenges.Challenge')
+    description = models.TextField()
+    file = models.FileField(upload_to='challenges')
