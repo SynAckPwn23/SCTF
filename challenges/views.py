d3ddd91b6de5c3d418320c5fdab46db27638b24b
Filippo <filippo.schiavio@gnucoop.com>
2017-05-04 19:17:56 +0200

    challenges views / initials data

--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from challenges.models import Challenge, Category
 
 
 @login_required
 def index(request):
-    parameters = {}
+    parameters = {
+        'challenges': Challenge.objects.all(),
+        'categories': Category.objects.all()
+    }
 
     return render(request, 'challenges/index.html', parameters)
 
