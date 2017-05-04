aea8b4976d45198f50f8d1127656229e2bcdd70e
Filippo <filippo.schiavio@gnucoop.com>
2017-05-04 16:48:23 +0200

    count users

--- sctf/views.py
+++ sctf/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 @login_required
 def index(request):
-    return render(request, 'base.html')
+    parameters = {
+        'users_count': get_user_model().objects.count(),
+    }
+    return render(request, 'base.html', parameters)
 
