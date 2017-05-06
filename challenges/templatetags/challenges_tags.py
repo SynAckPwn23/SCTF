00bf1a74192f9675a9546018b5fda95c97b37354
flpo <filippo.schiavio@gmail.com>
2017-05-06 17:03:59 +0200

    ...

--- challenges/templatetags/challenges_tags.py
+++ challenges/templatetags/challenges_tags.py
@@ -8,3 +8,8 @@ def btn_challenge_class(current_user, challenge):
     if current_user.solved_challenges.filter(pk=challenge.pk).exists():
         return 'btn-success'
     return 'btn-primary'
+
+
+@register.assignment_tag(takes_context=True)
+def challenge_is_solved(context, *args):
+    return context['user'].solved_challenges.filter(pk=context['challenge'].pk).exists()
