46a1fa6c437f6dae45917fae89e266cc2fab959e
Daniele <vottadaniele@gmail.com>
2017-05-06 20:31:31 +0200

    Challenges Count

--- sctf/views.py
+++ sctf/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from accounts.models import Team
+from challenges.models import Challenge
 
 
 @login_required
@@ -10,6 +11,7 @@ def index(request):
     parameters = {
         'users_count': get_user_model().objects.count(),
         'teams_count': Team.objects.count(),
+        'challenges_count': Challenge.objects.count(),
     }
     return render(request, 'sctf/base.html', parameters)
 
