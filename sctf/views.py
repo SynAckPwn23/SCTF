89b837028bdba2e001d000dc43bb9c8d860fe7bc
Daniele <vottadaniele@gmail.com>
2017-05-06 21:17:27 +0200

    Points Count

--- sctf/views.py
+++ sctf/views.py
@@ -2,8 +2,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from django.db.models import Sum
+
 from accounts.models import Team
 from challenges.models import Challenge
+
+
 
 
 @login_required
@@ -12,6 +16,7 @@ def index(request):
         'users_count': get_user_model().objects.count(),
         'teams_count': Team.objects.count(),
         'challenges_count': Challenge.objects.count(),
+        'points_count': Challenge.objects.aggregate(Sum('points')).values(),
     }
     return render(request, 'sctf/base.html', parameters)
 
