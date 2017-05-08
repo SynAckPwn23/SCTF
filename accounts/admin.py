256dc0032f855f98f273224140411136629b7205
Filippo <filippo.schiavio@gnucoop.com>
2017-05-08 17:08:49 +0200

    user team

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-from .models import Team
+from .models import Team, UserProfile
+
 
 class TabularUserAdmin(admin.TabularInline):
     model = get_user_model()
 
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     #filter_horizontal = ('TabularUserAdmin',)
+    pass
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
     pass
