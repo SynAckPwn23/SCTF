7ca8f63ade38ad135f59489d3e87a270f708b13d
Filippo <filippo.schiavio@gnucoop.com>
2017-05-08 18:22:18 +0200

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^', include('registration.backends.simple.urls')),
     url(r'^', include('registration.auth_urls')),
-    url(r'^teams/$', views.team, name='teams'),
+    url(r'^teams/$', views.index, name='teams'),
     url(r'^team/$', views.team, name='team'),
-    url(r'^team/(?P<pk>\w+)$', views.index, name='team'),
+    url(r'^team/(?P<pk>\w+)$', views.team, name='team'),
 ]
