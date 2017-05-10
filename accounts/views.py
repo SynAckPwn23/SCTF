28cd78d1d5657b076011e5e7fbcf75123f434bd6
Daniele <vottadaniele@gmail.com>
2017-05-10 22:29:40 +1400

    Ranking Users

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from accounts.models import Team
+from challenges.models import Challenge
+
 
 
 @login_required
@@ -17,7 +19,11 @@ def index(request):
 def team(request, pk=None):
     team = request.user.profile.team if pk is None else Team.objects.get(pk=pk)
     parameters = {
-        'team': team
+        'team': team,
+        'total_points_count': Challenge.objects.total_points(),
+        'teams_count': Team.objects.count(),
+
+
     }
 
     return render(request, 'accounts/team.html', parameters)
