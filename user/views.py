e365b8e70b7489363956bdc9a27e0881546bc7a8
Daniele <vottadaniele@gmail.com>
2017-05-17 09:49:51 +0200

    User App

--- user/views.py
+++ user/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
 @login_required
 def index(request):
-    parameters = {
-       
-    }
-    return render(request, 'user/index.html', parameters)
+	user = request.user
+	parameters = {
+	}
+	return render(request, 'user/index.html', parameters)
