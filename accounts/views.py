58eaa221e30033425ec7468f3deec722b9b8e258
Filippo <filippo.schiavio@gnucoop.com>
2017-05-18 19:20:26 +0200

    ...

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from accounts.models import Team
-from challenges.models import Challenge
+from challenges.models import Challenge, ChallengeSolved
 
+import json
 
 
 @login_required
@@ -18,11 +20,20 @@ def index(request):
 @login_required
 def team(request, pk=None):
     team = request.user.profile.team if pk is None else Team.objects.get(pk=pk)
+
+
+
+
+    time_points = defaultdict(lambda : 0)
+    for solved in ChallengeSolved.objects.filter(user__profile__team=team):
+        time_points[solved.datetime.timestamp()] += solved.challenge.points
+
     parameters = {
         'team': team,
         'total_points_count': Challenge.objects.total_points(),
         'teams_count': Team.objects.count(),
 
+        'time_points': json.dumps(sorted(time_points.items())),
 
     }
 
