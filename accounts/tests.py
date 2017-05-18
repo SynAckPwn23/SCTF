a900e99183b57760e6c741c8b88c7c317421d238
Daniele <vottadaniele@gmail.com>
2017-05-18 10:05:27 +0200

    Test

--- accounts/tests.py
+++ accounts/tests.py
@@ -11,10 +11,16 @@ class RegistrationTestCase(TestCase):
         response = self.client.post('/accounts/register/', {
             'username': 'username',
             'email': 'username@username.it',
+            'first_name': 'FirstName',
+            'last_name': 'LastName',
             'password1': 'u1u2u3u4',
             'password2': 'u1u2u3u4',
-            'first_name': 'UserName'
+           
         })
         u = get_user_model().objects.first()
         self.assertEqual(u.username, 'username')
-        self.assertEqual(u.first_name, 'UserName')
+        self.assertEqual(u.email, 'username@username.it')
+        self.assertEqual(u.first_name, 'FirstName')
+        self.assertEqual(u.last_name, 'LastName')
+        self.assertEqual(u.password1, 'u1u2u3u4')
+        self.assertEqual(u.password2, 'u1u2u3u4')
