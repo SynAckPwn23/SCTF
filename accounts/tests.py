901dbade4df950dc1355e19e88e6862d78ea882c
Daniele <vottadaniele@gmail.com>
2017-05-18 10:54:31 +0200

    Test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 
-
-
 class RegistrationTestCase(TestCase):
     def setUp(self):
         self.client = Client()
