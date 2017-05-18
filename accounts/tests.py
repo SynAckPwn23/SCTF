6d8142678e9e41155d293734c17948247c406895
Filippo <filippo.schiavio@gnucoop.com>
2017-05-18 23:41:34 +0200

    test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+
+class RegistrationTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.post('/accounts/register/', {
+            'username': 'username',
+            'email': 'username@username.it',
+            'password1': 'u1u2u3u4',
+            'password2': 'u1u2u3u4',
+            'first_name': 'UserName'
+        })
+        u = get_user_model().objects.first()
+        self.assertEqual(u.username, 'username')
+        self.assertEqual(u.first_name, 'UserName')
