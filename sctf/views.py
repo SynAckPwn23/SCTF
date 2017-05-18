575abe266063b077bfe6a74603f1c32a446d61a8
Daniele <vottadaniele@gmail.com>
2017-05-18 23:28:13 +0200

    "Test"

--- sctf/views.py
+++ sctf/views.py
@@ -1,15 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
 from django.db.models import Sum
-
 from accounts.models import Team
 from challenges.models import Challenge
-
-
-
-
 
 @login_required
 def index(request):
