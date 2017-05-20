ad2799f757dcdaea1f0936c80e5c4b414b258414
flpo <filippo.schiavio@gmail.com>
2017-05-20 10:00:07 +0200

    move sctf to SCTF

---
+++ reset.sh
@@ -0,0 +1,5 @@
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+rm db.sqlite3
+python manage.py makemigrations
+python manage.py migrate
