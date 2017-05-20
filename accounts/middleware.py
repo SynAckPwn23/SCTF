9e8b8be7031d00218d672db8bb32d868b71fded5
flpo <filippo.schiavio@gmail.com>
2017-05-20 12:00:39 +0200

    fix users middleware

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -2,6 +2,10 @@ from django.http import HttpResponseRedirect
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework.reverse import reverse
 
+from django.utils.functional import curry
+from django.views.defaults import *
+
+
 
 class LoggedInUserWithoutProfileMiddleware(MiddlewareMixin):
     redirect_url = reverse('no_profile')
@@ -10,20 +14,21 @@ class LoggedInUserWithoutProfileMiddleware(MiddlewareMixin):
         if request.user.is_authenticated() and \
                 not hasattr(request.user, 'profile') and \
                 request.path != self.redirect_url:
-            return HttpResponseRedirect(self.redirect_url)
+            handler500 = curry(server_error, template_name='accounts/500_no_profile.html')
+            return handler500(request)
 
 
 class LoggedInUserWithoutTeamMiddleware(MiddlewareMixin):
     redirect_url = reverse('no_team')
 
     allowed_paths = [
-        redirect_url,
         reverse('no_profile'),
-        # TODO set pages allowed to users without team (ie user profile)
+        reverse('no_team'),
+        # TODO set pages allowed to users without team (ie: manage user profile)
     ]
 
     def process_request(self, request):
-        if request.path in self.allowed_paths:
-            return
-        if request.user.is_authenticated() and request.user.profile.team is None:
+        if request.path not in self.allowed_paths and \
+                request.user.is_authenticated() and \
+                request.user.profile.team is None:
             return HttpResponseRedirect(self.redirect_url)
