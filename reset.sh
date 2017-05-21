b1e3684be36dad5baf08b3930a50bd5bef21647b
flpo <filippo.schiavio@gmail.com>
2017-05-21 18:12:28 +0200

    reset script

--- reset.sh
+++ reset.sh
@@ -6,9 +6,28 @@ find . -path "*/migrations/*.pyc" -not -path "./venv/*" -delete
 rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
-# Admin
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+
+python manage.py loaddata locations.json
+
+
 # Test Data
+python manage.py loadtestdata accounts.Team:30 -u accounts.fixtures.autofixtures.TeamAutoFixture
 python manage.py loadtestdata auth.User:100 -u accounts.fixtures.autofixtures.UserAutoFixture
-python manage.py loadtestdata accounts.Team:29 -u accounts.fixtures.autofixtures.TeamAutoFixture
 python manage.py loadtestdata accounts.UserProfile:100
+
+# Admin
+echo "
+from django.contrib.auth.models import User;
+from accounts.models import UserProfile, Team;
+from cities_light.models import Country;
+admin=User.objects.create_superuser('admin', 'admin@admin.com', 'admin');
+team=Team.objects.create(name='admin')
+
+UserProfile.objects.create(
+    user=admin,
+    job='job',
+    gender='M',
+    country=Country.objects.get(name='Italy'),
+    team=team
+);
+" | python manage.py shell
