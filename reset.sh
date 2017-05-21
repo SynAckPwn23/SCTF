17f756062e574345f4434b4dbac6668dbca5c233
Daniele <vottadaniele@gmail.com>
2017-05-21 15:34:28 +0200

    Reset

--- reset.sh
+++ reset.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Clean Environment
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
+find . -path "*/migrations/*.py" -not -name "__init__.py" -not -path "./venv/*" -delete
+
+find . -path "*/migrations/*.pyc" -not -path "./venv/*" -delete
 rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
