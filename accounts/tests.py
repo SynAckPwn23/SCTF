7072223d10b376f41d7b95053529c9f0fec9ce13
flpo <filippo.schiavio@gmail.com>
2017-05-21 11:07:05 +0200

    all tests fixed

--- accounts/tests.py
+++ accounts/tests.py
@@ -19,51 +19,49 @@ class RegistrationTestCase(TestCase, SimpleTestCase):
         )
         self.country = Country.objects.create(name='Italy')
 
-
     #Test User Login
     def test_login(self):
-        response = self.client.post('/accounts/login/', {
+        response = self.client.post(reverse('auth_login'), {
             'username': 'username',
             'password': 'u1u2u3u4',
         })
-        u = get_user_model().objects.first()
-        # Correct username and password
-        self.assertEqual(u.username, 'username')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
+        u = get_user_model().objects.get(username='username')
+        self.assertEqual(int(self.client.session['_auth_user_id']), u.pk)
 
-    #Test User Login Empty
+    # Test User Login Empty
     def test_login_empty(self):
-        response = self.client.post('/accounts/login/', {
+        response = self.client.post(reverse('auth_login'), {
             'username': 'username',
-            'password': 'u1u2u3u4',
-            })
-        u = get_user_model().objects.first()
-        # Correct username and password
-        self.assertEqual(u.username, '')
-        self.assertEqual(u.password, '')
-        self.assertIn('This value is required.', response.content)
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Your username and password didn\'t match.')
 
-    #Test Reset Password
+    # Test User Login Empty
+    def test_login_wrong(self):
+        response = self.client.post(reverse('auth_login'), {
+            'username': 'username',
+            'password': 'wrong',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Your username and password didn\'t match.')
+
+
+    # Test Reset Password
     def test_reset(self):
-        response = self.client.post('/accounts/password/reset', {
+        response = self.client.post(reverse('auth_password_reset'), {
             'email': 'username@username.it',
-            })
-        u = get_user_model().objects.first()
-        # Correct email
-        self.assertEqual(u.email, 'username@username.it')
+        })
+        self.assertEqual(response.status_code, 302)
+
+
+    # Test Reset Password Empty
+    def test_reset_empty(self):
+        response = self.client.post(reverse('auth_password_reset'), {})
         self.assertEqual(response.status_code, 200)
 
-    #Test Reset Password Empty
-    def test_reset_empty(self):
-        response = self.client.post('/accounts/password/reset', {
-            'email': 'username@username.it',
-            })
-        u = get_user_model().objects.first()
-        # Wrong email
-        self.assertEqual(u.email, '')
-        self.assertIn('This value is required.', response.content)
 
-    #Test User Registration
+    # Test User Registration
     def test_register(self):
         response = self.client.post(reverse('registration_register'), {
             'username': 'new_username',
@@ -73,8 +71,9 @@ class RegistrationTestCase(TestCase, SimpleTestCase):
             'password1': 'u1u2u3u4',
             'password2': 'u1u2u3u4',
             'job': 'job',
+            'gender': 'M',
             'country': 1,
-            })
+        })
         # Created
         self.assertEqual(response.status_code, 302)
 
@@ -87,40 +86,31 @@ class RegistrationTestCase(TestCase, SimpleTestCase):
         self.assertEqual(u.first_name, 'FirstName')
         # Correct last_name
         self.assertEqual(u.last_name, 'LastName')
+        # Correct Job
+        self.assertEqual(u.profile.job, 'job')
+        # Correct Gender
+        self.assertEqual(u.profile.gender, 'M')
         # Correct country
         self.assertEqual(u.profile.country, self.country)
 
 
-    #Test User Registration Empty
+    # Test User Registration Empty
     def test_register_empty(self):
-        response = self.client.post('/accounts/register/', {
-            'username': 'username',
-            'email': 'username@username.it',
-            'first_name': 'FirstName',
-            'last_name': 'LastName',
-            'password1': 'u1u2u3u4',
-            'password2': 'u1u2u3u4',
-            })
-        u = get_user_model().objects.first()
-        # Empty username
-        self.assertEqual(u.username, '')
-        # Empty email
-        self.assertEqual(u.email, '')
-        # Empty first_name
-        self.assertEqual(u.first_name, '')
-        # Empty last_name
-        self.assertEqual(u.last_name, '')
-        # Empty password1
-        self.assertEqual(u.password1, '')
-        # Empty pasword2
-        self.assertEqual(u.password2, '')
-        self.assertIn('This value is required.', response.content)
+        response = self.client.post(reverse('registration_register'), {})
+
+        # Not created
+        self.assertEqual(response.status_code, 200)
+
+        # TODO: test this for all required fields
+        self.assertContains(response, 'This field is required')
 
 
     def test_login_without_profile(self):
-        self.client.login(username='temporary', password='temporary')
-        response = self.client.get('/', follow=True)
+        response = self.client.post(reverse('auth_login'), {
+            'username': 'temporary',
+            'password': 'temporary',
+        })
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 500)
-        self.assertIn('User without profile', response.content)
 
 
