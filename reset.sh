35a73d7f9ca68e0e2e59f9d62f02bb2b24a52876
Daniele <vottadaniele@gmail.com>
2017-05-21 15:25:36 +0200

    Reset script

--- reset.sh
+++ reset.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+# Clean Environment
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
+# Admin
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+# Test Data
+python manage.py loadtestdata auth.User:100 -u accounts.fixtures.autofixtures.UserAutoFixture
+python manage.py loadtestdata accounts.Team:29 -u accounts.fixtures.autofixtures.TeamAutoFixture
+python manage.py loadtestdata accounts.UserProfile:100
