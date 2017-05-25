6724098166bc3b915f1f64eb33bb570d1661a422
Filippo <filippo.schiavio@gnucoop.com>
2017-05-25 17:51:50 +0200

    prepare score tests

---
+++ SCTF/tests.py
@@ -0,0 +1,48 @@
+from itertools import chain, repeat
+
+from cities_light.models import Country
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from accounts.models import Team, UserProfile
+from challenges.models import Category, Challenge, ChallengeSolved
+
+
+class UserScoreTest(TestCase):
+    def setUp(self):
+        test_category = Category.objects.create(name='test')
+        country = Country.objects.create(name='Italy')
+
+        # create 3 teams: t0, t1, t2
+        self.teams = [Team.objects.create(name='t{}'.format(i)) for i in range(3)]
+
+        # create 9 users: u0, u1, ... u8
+        self.users = [get_user_model().objects.create_user(
+            'u{}'.format(i),
+            'u{}@test.com'.format(i),
+            'u1u2u3u4'
+        ) for i in range(9)]
+
+        # teams - users association: t0: (u0, u1, u2), t1: (u3, u4, u5), t2: (u6, u7, u8)
+        teams_users = chain.from_iterable(repeat(t, 3) for t in self.teams)
+
+        # create users profile
+        for u in self.users:
+            UserProfile.objects.create(user=u, job='job', gender='M', country=country, team=next(teams_users))
+
+        # create 9 challenges: c0, c1, ..., c8
+        self.challenges = [
+            Challenge.objects.create(name='c{}'.format(i), points=i, category=test_category)
+            for i in range(9)
+        ]
+
+        # solved challenges: each user u_i solves all challenges from c_0 to c_i (ie: u2 solves c0,c1,c2)
+        for i, u in enumerate(self.user, 1):
+            for c in self.challenges[:i]:
+                ChallengeSolved.objects.create(user=u, challenge=c)
+
+
+    def test_users_score(self):
+        for i, u in self.users:
+            self.assertEqual(u.points, sum(c.points for c in self.challenges[:i]))
+
--- challenges/tests.py
+++ challenges/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from challenges.models import Category
+
+
+class ChallengesTest(TestCase):
+
+    def setUp(self):
+        self.test_category = Category.objects.create(name='test')
+        self.u1, self.u2, self.u3 = (get_user_model().objects.create_user(
+            u,
+            u + '@u.it',
+            'u1u2u3u4'
+        ) for u in ('u1', 'u2', 'u3'))
+
