264874d0cee2c0dcbca82602ef349b3fae1f6698
Daniele <vottadaniele@gmail.com>
2017-06-19 23:19:27 +0200

    Disabled PNotify in custom.js

--- SCTF/static/sctf/build/js/custom.js
+++ SCTF/static/sctf/build/js/custom.js
@@ -1474,7 +1474,7 @@ if (typeof NProgress != 'undefined') {
 
   };
 
-  /* PNotify */
+  /* PNotify 
 
   function init_PNotify() {
 
@@ -1504,7 +1504,7 @@ if (typeof NProgress != 'undefined') {
   	});
 
   }; 
-
+*/
 
   /* CUSTOM NOTIFICATION */
 
