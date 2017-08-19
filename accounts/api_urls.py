77c1a1215c3a64e4ac91a98d31d0e8f45b4044b5
Filippo <filippo.schiavio@gnucoop.com>
2017-08-19 15:54:54 +0200

    fix teams

--- accounts/api_urls.py
+++ accounts/api_urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .views import TeamCreateViewSet
 
 router = routers.SimpleRouter()
-router.register(r'^team/create/$', TeamCreateViewSet, 'team_create')
+router.register(r'team-create', TeamCreateViewSet, 'team')
 
 urlpatterns = router.urls
