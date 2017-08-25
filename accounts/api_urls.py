2073195cff95baf4df281826057fdcbc57428c47
Filippo <filippo.schiavio@gnucoop.com>
2017-08-25 18:53:53 +0200

    endpoints

--- accounts/api_urls.py
+++ accounts/api_urls.py
@@ -1,7 +1,14 @@
-from rest_framework import routers
-from .views import UserTeamRequestViewSet
+#from rest_framework import routers
+from django.conf.urls import url
 
+from .views import UserRequestHTMLTable
+
+'''
 router = routers.SimpleRouter()
 router.register(r'user-team-request', UserTeamRequestViewSet, 'user-team-request')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    
+]
+'''
+urlpatterns = [url('user-team-request', UserRequestHTMLTable.as_view())]
