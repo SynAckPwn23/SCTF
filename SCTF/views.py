32c6c627b7d486f9fcfa0a92974eb37629ca94bb
Filippo <filippo.schiavio@gnucoop.com>
2017-08-26 14:21:42 +0200

    django constance

--- SCTF/views.py
+++ SCTF/views.py
@@ -1,11 +1,14 @@
 import json
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import user_passes_test
 from django.db.models import Count
 from django.shortcuts import render
 from accounts.models import Team
 from challenges.models import Challenge
 from cities_light.models import Country
+from constance import config
+
 
 def index(request):
     user = request.user
@@ -20,7 +23,20 @@ def index(request):
         'challenges_count': Challenge.objects.count(),
         'total_points_count': Challenge.objects.total_points(),
         'user_points_count': user.profile.total_points,
-        'countries_users': json.dumps({c.code2.lower(): c.num_user for c in countries})
+        'countries_users': json.dumps({c.code2.lower(): c.num_user for c in countries}),
+        'config': config
     }
     return render(request, 'sctf/base.html', parameters)
 
+
+@user_passes_test(lambda u: u.is_superuser)
+def game_play(request):
+    from datetime import datetime
+    # TODO check status
+    config.GAME_START_DATETIME = datetime.now()
+    config.GAME_STATUS = 'PLAY'
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def game_pause(request):
+    pass
