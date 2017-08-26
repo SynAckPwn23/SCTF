e4105de38d7c18d6c626c699328e176fdbc092f1
Filippo <filippo.schiavio@gnucoop.com>
2017-08-26 23:45:44 +0200

    fix

--- SCTF/middleware.py
+++ SCTF/middleware.py
@@ -8,5 +8,5 @@ from constance import config
 class FilterRequestByGameStateMiddlewareMixin(MiddlewareMixin):
 
     def process_request(self, request):
-        if 'challenge' in request.path and config.GAME_STATUS == 'SETUP':
+        if 'challenge' in request.path and config.GAME_STATUS != 'PLAY':
             return HttpResponseRedirect('/')
