2043e8d5d9b0c4977bbf6a64165ff6f6eb3d730b
Daniele <vottadaniele@gmail.com>
2018-11-11 18:31:12 +0100

    Code Format

--- SCTF/views.py
+++ SCTF/views.py
@@ -1,5 +1,4 @@
 import json
-
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import user_passes_test
@@ -7,20 +6,16 @@ from django.db.models import Count
 from django.shortcuts import render, redirect
 from datetime import timedelta
 from django.utils import timezone
-
 from SCTF.utils import set_game_duration, send_pause_message, send_start_message, send_resume_message, send_end_message, send_reset_message
 from accounts.models import Team, UserProfile
 from challenges.models import Challenge, ChallengeSolved, ChallengeFail
 from cities_light.models import Country
 from constance import config
 
-
-
 def index(request):
     countries = Country.objects\
         .annotate(num_user=Count('userprofile'))\
         .filter(num_user__gt=0)
-
     parameters = {
         'users_count': get_user_model().objects.count(),
         'teams_count': Team.objects.count(),
@@ -31,10 +26,8 @@ def index(request):
     }
     return render(request, 'sctf/base.html', parameters)
 
-
 def _return_back_redirect(request):
     return redirect('/')
-
 
 @user_passes_test(lambda u: u.is_superuser)
 def game_play(request):
@@ -49,7 +42,6 @@ def game_play(request):
     config.GAME_START_DATETIME = timezone.now()
     return _return_back_redirect(request)
 
-
 @user_passes_test(lambda u: u.is_superuser)
 def game_pause(request):
     if config.GAME_STATUS == settings.GAME_STATUS_PLAY:
@@ -57,7 +49,6 @@ def game_pause(request):
         config.GAME_STATUS = settings.GAME_STATUS_PAUSE
         set_game_duration(timezone.now() - config.GAME_START_DATETIME)
     return _return_back_redirect(request)
-
 
 @user_passes_test(lambda u: u.is_superuser)
 def game_end(request):
@@ -67,13 +58,11 @@ def game_end(request):
         set_game_duration(timedelta())
     return _return_back_redirect(request)
 
-
 @user_passes_test(lambda u: u.is_superuser)
 def game_reset(request):
     send_reset_message()
     _reset_game_status_challenges()
     return _return_back_redirect(request)
-
 
 @user_passes_test(lambda u: u.is_superuser)
 def game_reset_users(request):
@@ -85,7 +74,6 @@ def game_reset_users(request):
     _reset_game_status_challenges()
     return _return_back_redirect(request)
 
-
 def _reset_game_status_challenges():
     config.GAME_STATUS = settings.GAME_STATUS_SETUP
     config.GAME_DURATION_DAYS = settings.CONSTANCE_CONFIG['GAME_DURATION_DAYS'][0]
@@ -94,21 +82,17 @@ def _reset_game_status_challenges():
     ChallengeSolved.objects.all().delete()
     ChallengeFail.objects.all().delete()
 
-
 def game_paused_view(request):
     if config.GAME_STATUS != settings.GAME_STATUS_PAUSE:
         return _return_back_redirect(request)
     return render(request, 'sctf/game_paused.html')
-
 
 def game_stopped_view(request):
     if config.GAME_STATUS != settings.GAME_STATUS_FINISH:
         return _return_back_redirect(request)
     return render(request, 'sctf/game_stopped.html')
 
-
 def game_setup_state_view(request):
     if config.GAME_STATUS != settings.GAME_STATUS_SETUP:
         return _return_back_redirect(request)
     return render(request, 'sctf/game_setup.html')
-
